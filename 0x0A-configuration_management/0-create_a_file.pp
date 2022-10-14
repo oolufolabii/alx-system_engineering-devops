1580ab668364756ac4491e3837212f454d8b609f
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-14 09:04:17 +0100

    Configuration Management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creating a file
 
 file { '/tmp/school':
-  mode   => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
