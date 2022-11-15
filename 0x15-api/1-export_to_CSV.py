2f53575c80fb2e2cb544d213ac47621e37b4aa8a
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-15 04:18:13 +0100

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,10 +13,12 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             user_id = int(sys.argv[1])
-            user_request = requests.get('{}/users/{}'.format(API_URL, user_id)).json()
+            user_request = requests.get('{}/users/{}'.format(
+                API_URL, user_id)).json()
             todos_request = requests.get('{}/todos'.format(API_URL)).json()
             user_name = user_request.get('username')
-            todos = list(filter(lambda x: x.get('userId') == user_id, todos_request))
+            todos = list(filter(
+                lambda x: x.get('userId') == user_id, todos_request))
             with open('{}.csv'.format(user_id), 'w') as file:
                 for todo in todos:
                     file.write(
