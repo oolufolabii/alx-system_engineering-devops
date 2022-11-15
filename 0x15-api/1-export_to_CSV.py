27397ca38eb7b8d1901fc045bb6e2b0dda97bf70
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-15 04:11:01 +0100

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """Python script to export data in the CSV format."""
 
+import re
+import requests
+import sys
+
 
 API_URL = 'https://jsonplaceholder.typicode.com'
 
-if __name__ == "__main__":
-    import csv
-    import requests
-    import sys
 
-    userId = sys.argv[1]
-    user = requests.get("{}/users/{}".format(API_URL, userId))
-
-    name = user.json().get('name')
-    todos = requests.get('{}/todos'.format(API_URL))
-
-    file_name = userId + '.csv'
-    with open(file_name, mode='w') as file:
-        csv_writer = csv.writer(file, delimiter=',', quotechar='"',
-                                quoting=csv.QUOTE_ALL, lineterminator='\n')
-
-        for task in todos.json():
-            if task.get('userId') == int(userId):
-                csv_writer.writerow([userId, name, str(task.get('completed')),
-                                     task.get('title')])
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            user_id = int(sys.argv[1])
+            user_request = requests.get('{}/users/{}'.format(API_URL, user_id)).json()
+            todos_request = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_request.get('username')
+            todos = list(filter(lambda x: x.get('userId') == user_id, todos_request))
+            with open('{}.csv'.format(user_id), 'w') as file:
+                for todo in todos:
+                    file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            user_id,
+                            user_name,
+                            todo.get('completed'),
+                            todo.get('title')
+                        )
+                    )
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
 """Python script to export data in the JSON format."""
+import json
+import re
+import requests
+import sys
 
 
 API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
 
-if __name__ == "__main__":
 
-    import requests
-    import sys
-    import json
-
-    userId = sys.argv[1]
-    user = requests.get("{}/users/{}".format(API_URL, userId))
-
-    name = user.json().get('name')
-    todos = requests.get('{}/todos'.format(API_URL))
-
-    todos_json = todos.json()
-
-    user_todo = {}
-    task_list = []
-
-    for task in todos_json:
-        if task.get('userId') == int(userId):
-            task_dict = {"task": task.get('title'),
-                         "completed": task.get('completed'),
-                         "username": user.json().get('username')}
-            task_list.append(task_dict)
-    user_todo[userId] = task_list
-
-    file_name = userId + '.json'
-    with open(file_name, mode='w') as file:
-        json.dump(user_todo, file)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            user_id = int(sys.argv[1])
+            user_request = requests.get(
+                '{}/users/{}'.format(API_URL, user_id)).json()
+            todos_request = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_request.get('username')
+            todos = list(filter(lambda x: x.get(
+                'userId') == user_id, todos_request))
+            with open('{}.json'.format(user_id), 'w') as file:
+                user_data = list(map(
+                    lambda x: {
+                        'task': x.get('title'),
+                        'completed': x.get('completed'),
+                        'username': user_name
+                    },
+                    todos
+                ))
+                users_data = {'{}'.format(user_id): user_data}
+                json.dump(users_data, file)
