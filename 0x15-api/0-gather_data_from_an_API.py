8d7dfa6a8520a3043bbef15b1202564df1e8a85c
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-15 04:05:23 +0100

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,17 +9,18 @@ import sys
 
 
 API_URL = 'https://jsonplaceholder.typicode.com'
-'''The API's URL.'''
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            user_request = requests.get(
+                '{}/users/{}'.format(API_URL, id)).json()
+            todos_request = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_request.get('name')
+            todos = list(filter(lambda x: x.get(
+                'userId') == id, todos_request))
             todos_done = list(filter(lambda x: x.get('completed'), todos))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
