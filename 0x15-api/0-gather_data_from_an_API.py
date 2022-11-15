5dde620238d3adae45c50a3512e8bef251890421
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-15 04:03:59 +0100

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,30 +3,30 @@
 about his/her To-do list progress.
 '''
 
-if __name__ == "__main__":
+import re
+import requests
+import sys
 
-    import re
-    import requests
-    import sys
 
-    API_URL = 'https://jsonplaceholder.typicode.com'
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
 
+
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            user_id = int(sys.argv[1])
-
-        user = requests.get('{}/users/{}'.format(API_URL, user_id)).json()
-        name = user.get('name')
-        todos_req = requests.get('{}/todos'.format(API_URL)).json()
-
-        todos = list(filter(lambda x: x.get('userId') == id, todos_req))
-        todos_done = list(filter(lambda x: x.get('completed'), todos))
-        print(
-            'Employee {} is done with tasks({}/{}):'.format(
-                name,
-                len(todos_done),
-                len(todos)
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
             )
-        )
-        for todo_done in todos_done:
-            print('\t {}'.format(todo_done.get('title')))
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
